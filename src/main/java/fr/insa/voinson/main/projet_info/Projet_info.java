283713377f6a2cfed8b1b4785a6e6b2c1a269642
rvoinson01 <rvoinson01@SEC-C212-F12.csip.in>
2025-04-02 08:55:58 +0200

    First commit

---
+++ src/main/java/fr/insa/voinson/main/projet_info/Projet_info.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.insa.voinson.main.projet_info;
+
+/**
+ *
+ * @author rvoinson01
+ */
+public class Projet_info {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
